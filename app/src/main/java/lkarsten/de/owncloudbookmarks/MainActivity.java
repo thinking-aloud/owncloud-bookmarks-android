221a9e03a4bd5f89734244c80493871ed8c0e000
Lennart K <lennart.k@thinking-aloud.eu>
2016-08-14 02:51:36 +0200

    first working version, that shows bookmarks
    removed the ownCloud-library

--- app/src/main/java/lkarsten/de/owncloudbookmarks/MainActivity.java
+++ app/src/main/java/lkarsten/de/owncloudbookmarks/MainActivity.java
@@ -1,29 +1,164 @@
 package lkarsten.de.owncloudbookmarks;
 
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import com.owncloud.android.lib.common.OwnCloudClient;
-import com.owncloud.android.lib.common.OwnCloudClientFactory;
-import com.owncloud.android.lib.common.OwnCloudCredentialsFactory;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
-
-    private OwnCloudClient mClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Uri serverUri = Uri.parse(getString(R.string.server_base_url));
-        mClient = OwnCloudClientFactory.createOwnCloudClient(serverUri, this, true);
-        mClient.setCredentials(
-                OwnCloudCredentialsFactory.newBasicCredentials(
-                        getString(R.string.username),
-                        getString(R.string.password)
-                )
-        );
+        final String BASE_URL = "https://lkarsten.de/owncloud/index.php/apps/bookmarks/public/rest/v1/bookmark";
+        String request = "?user=test&password=test";
+
+        try {
+            String response = new LongOperation().execute(BASE_URL + request).get();
+
+            final ArrayList<String> list = new ArrayList<>();
+            JSONArray json = new JSONArray(response);
+            for (int i = 0; i < json.length(); i++) {
+                JSONObject object = json.getJSONObject(i);
+                list.add(object.get("url").toString());
+            }
+
+            final ListView listView = (ListView) findViewById(R.id.listView);
+            final StableArrayAdapter adapter = new StableArrayAdapter(this,
+                    android.R.layout.simple_list_item_1, list);
+            listView.setAdapter(adapter);
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, final View view,
+                                        int position, long id) {
+                    final String item = (String) parent.getItemAtPosition(position);
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                        view.animate().setDuration(2000).alpha(0)
+                                .withEndAction(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        list.remove(item);
+                                        adapter.notifyDataSetChanged();
+                                        view.setAlpha(1);
+                                    }
+                                });
+                    }
+                }
+            });
+
+        } catch (InterruptedException | ExecutionException | JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
+class StableArrayAdapter extends ArrayAdapter<String> {
+
+    HashMap<String, Integer> mIdMap = new HashMap<>();
+
+    public StableArrayAdapter(Context context, int textViewResourceId,
+                              List<String> objects) {
+        super(context, textViewResourceId, objects);
+        for (int i = 0; i < objects.size(); ++i) {
+            mIdMap.put(objects.get(i), i);
+        }
+    }
+
+    @Override
+    public long getItemId(int position) {
+        String item = getItem(position);
+        return mIdMap.get(item);
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+}
+
+class LongOperation extends AsyncTask<String, Void, String> {
+
+    @Override
+    protected String doInBackground(String... params) {
+
+        try {
+            return downloadContent(params[0]);
+        } catch (IOException e) {
+            return "Unable to retrieve data. URL may be invalid.";
+        }
+
+    }
+
+    private String downloadContent(String requestUrl) throws IOException {
+        InputStream inputStream = null;
+
+        try {
+            URL url = new URL(requestUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setReadTimeout(10000);
+            connection.setConnectTimeout(15000);
+            connection.setRequestMethod("GET");
+            connection.setDoInput(true);
+            connection.connect();
+            inputStream = connection.getInputStream();
+
+            // Convert the InputStream into a string
+            return convertInputStreamToString(inputStream);
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+    }
+
+    public String convertInputStreamToString(InputStream inputStream) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder result = new StringBuilder(inputStream.available());
+        String line;
+        while ((line = reader.readLine()) != null) {
+            result.append(line).append("\n");
+        }
+
+        return result.toString();
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+    }
+
+    @Override
+    protected void onPreExecute() {
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
     }
 }
